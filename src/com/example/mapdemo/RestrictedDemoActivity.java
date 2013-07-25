b98d84088a58dda7428831d6dbd7f7689a0d4dda
Hua Minh Quang <huaminhquang@gmail.com>
2013-07-25 12:18:47 +0700

    First time commit

---
+++ src/com/example/mapdemo/RestrictedDemoActivity.java
@@ -0,0 +1,71 @@
+package com.example.mapdemo;
+
+import com.example.mapdemo.view.RestrictedMapView;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class RestrictedDemoActivity extends FragmentActivity{
+	private RestrictedMapView mMapView;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restricted_demo);
+
+        mMapView = (RestrictedMapView) findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((MapView) findViewById(R.id.map)).getMap();
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    @Override
+    protected void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+}
